4489db17d252f5bd2e52fe56b687b4ea3da0bfb7
See-ming Lee <accounts@seeminglee.com>
2011-11-02 05:31:51 -0400

    quick chekc-in.  almost done for File Info Widget

--- src/iv64_cocos.py
+++ src/iv64_cocos.py
@@ -13,11 +13,13 @@ Copyright (c) 2011 See-ming Lee. All rights reserved.
 import re
 import os
 import os.path
-import pyglet.gl
 from cocos.director import director
 from pyglet import event
 import cocos
 from pyglet.gl import gl
+import pyglet
+
+
 
 
 
@@ -56,227 +58,6 @@ class FileType(object):
 
 
 
-
-### Models _-------------------------------------------------------------------
-class SlideshowModel(event.EventDispatcher):
-	"""model of the slideshow"""
-	def __init__(self, folder):
-		"""initialize with name of the folder"""
-		try:
-			self.folder = folder
-			if not os.path.exists(self.folder):
-				raise InvalidFolderError('Path entered does not exist.')
-			if os.path.isfile(self.folder):
-				self.folder = os.path.dirname(self.folder)
-			self.folder = os.path.abspath(self.folder)
-		except InvalidFolderError as e:
-			print "An exception occurred: message: ", e.msg
-			self.folder = None
-
-		self.all_files = [f for f in os.listdir(self.folder)
-		                 if FileType.isnot_sys.match(f)]
-		self.image_files = [f for f in self.all_files
-		                   if FileType.is_image.match(f)]
-		self.files = self.image_files
-
-		self.loop = True
-		self.random = False
-
-		self._current_id = 0
-		self._direction = "forward"
-		self._play_random = False
-
-		# used for tracking random files. in particular, allow
-		# to go back to files just previously viewed when playing in random
-		self._play_random = False
-		self._id_queue_past = []
-		self._id_queue = []
-
-		# animation properties
-		self._autonext_interval_msec = 2000 # in milliseconds
-
-		# animation started
-		self._playing = False
-
-	@property
-	def direction(self):
-		return self._direction
-
-	@property
-	def play_random(self):
-		return self._play_random
-
-	@property
-	def current_id(self):
-		return  self._current_id
-
-	@property
-	def current_file(self):
-		if self.play_random:
-			filename = self.files.index(self.current_id)
-		else:
-			filename = self.files[self.current_id]
-		return os.path.join(self.folder, filename)
-
-	@property
-	def total_files(self):
-		return len(self.files)
-
-	@property
-	def current_image(self):
-		return Loader.load_image(self.current_file)
-
-#	@property
-#	def playing(self):
-#		return self._playing
-#
-#	def play(self):
-#		"""Schedule and start auto-advance to next slide"""
-#		print("play args:")
-#		print(args)
-#		pyglet.clock.schedule_once( self.play_next,
-#									self._autonext_interval_msec)
-#		# instead of using interval schedules, it just callls the same
-#		# function repeated so if the system is backed up it won't create
-#		# additional problems
-#		self._playing = True
-#
-#	def stop(self):
-#		"""Stop animation"""
-#		pyglet.clock.unschedule(self.play_next)
-#		self._playing = False
-#
-#	def play_next(self):
-#		"""auto advance to next"""
-#		print("plext_next() args:")
-#		print(args)
-#		self.next()
-#		self.play()
-#
-#	def toggle_play(self):
-#		"""toggle playing and not playing states"""
-#		if self._playing:
-#			self.stop()
-#			self._playing = False
-#		else:
-#			self.play()
-#			self._playing = True
-#
-#	def change_play_interval(self, msec):
-#		"""change the time interval between advances"""
-#		msec = max(msec, 500)   # at least 500msec
-#		msec = min(10000, msec) # at most 10s
-#		self.stop()
-#		self._autonext_interval_msec = msec
-#		self.start()
-
-
-	def next(self):
-		"""move to the next item"""
-		if not self.play_random:
-			# normal
-			if self.direction is "forward":
-				self._current_id += 1
-			else:
-				self._current_id -= 1
-			self.limit_id_range()
-
-		# random
-		else:
-			if not len(self._id_queue):
-				self._id_queue = range(len(self.files))
-			else:
-				self._current_id = random.randrange(len(self._id_queue))
-				self._id_queue.remove(self.current_id)
-				self._id_queue_past.append(self.current_id)
-
-		self._dispatch_update()
-
-	def prev(self):
-		"""move to the previous item"""
-		if not self.play_random:
-			# normal
-			if self.direction is "forward":
-				self._current_id -= 1
-			else:
-				self._current_id += 1
-			self.limit_id_range()
-
-		# random
-		else:
-			if not len(self._id_queue_past):
-				return # no more previous queue to go to, so don't do anything
-			else:
-				self._current_id = self._id_queue_past.pop()
-				self._id_queue.append(self.current_id)
-
-		self._dispatch_update()
-
-	def limit_id_range(self):
-		self._current_id = (self._current_id + len(self.files)) \
-				% len(self.files)
-
-	def _dispatch_update(self):
-		self.dispatch_event(
-		    "on_slideshow_model_update",
-		    dict(
-			    current_id   = self.current_id,
-		        current_file = self.current_file,
-		        total_files  = self.total_files
-		    )
-
-		)
-
-
-### Controllers ---------------------------------------------------------------
-
-class SlideshowController(event.EventDispatcher):
-	"""controller for slideshow interactions"""
-
-	# started = False
-
-	def __init__(self, model):
-		"""init control with model"""
-		self.model = model
-
-	def add_model_observer(self, view):
-		"""Made sure all the views will listen to any model updates"""
-		self.model.push_handlers(view)
-
-
-	def on_key_press(self, symbol, modifiers):
-		"""key press event: mostly listening for slideshow navigation"""
-		if symbol in [key.RIGHT, key.LEFT]:
-
-			if symbol == key.RIGHT:
-				self.model.next()
-			elif symbol == key.LEFT:
-				self.model.prev()
-
-		# slideshow start/stop
-		elif symbol in [key.S, key.SPACE]:
-			self.model.toggle_play()
-
-		# adjust show timing and starts it off if not already playing
-		else:
-			time = 10000
-			timekeys = (key.GRAVE, key._1, key._2, key._3, key._4, key._5,
-						key._6, key._7, key._8, key._9, key._0)
-			if symbol in timekeys:
-				time = timekeys.index(symbol) * 1000
-			if symbol == key.GRAVE:
-				time = 500
-			self.model.change_play_interval(time)
-
-
-	# def toggle_show(self):
-		# self.started = not self.started
-
-
-class SlideshowView(cocos.scene.Scene):
-	"""The main scene: where a single image is displayed at any given time,
-	showing its metadata and allowing the user to control the animation
-	manually all allows the scene to play with them automatically.."""
 
 
 class EnumUtil(object):
@@ -335,6 +116,7 @@ class Util_Scaler(object):
 
 class BackgroundLayer(cocos.layer.ColorLayer):
 	"""The absolute bottomest window."""
+
 	is_event_handler = True
 
 	def __init__(self, r, g, b, a, width=100, height=100):
@@ -481,6 +263,7 @@ Button.register_event_type('on_button_press')
 
 class TextWidgetLayer(Control):
 	"""The absolute bottomest window."""
+
 	is_event_handler = True
 
 	def __init__(self, r=0, g=0, b=0, a=255, width=100, height=100):
@@ -491,17 +274,60 @@ class TextWidgetLayer(Control):
 		super(TextWidgetLayer, self).__init__()
 
 
-class FileInfoWidget(Control):
-	"""Single line text display object for image meta data
-	when slide show is running."""
-	filename = ''
-	file_id = 0
-	file_total = 0
+#
+#class FileInfoWidget(Control):
+#	"""Single line text display object for image meta data
+#	when slide show is running."""
+#	filename = ''
+#	file_id = 0
+#	file_total = 0
+#
+#	is_event_handler = True
+#
+#	def __init__(self, parent, x=0, y=100, group=None, batch=None,
+#	             **kwargs):
+#
+#		super(FileInfoWidget, self).__init__()
 
-	def __init__(self, parent, x=0, y=100, group=None, batch=None,
-	             **kwargs):
 
-		super(FileInfoWidget, self).__init__()
+class FileInfoLayer(cocos.layer.base_layers.Layer):
+
+	is_event_handler = True
+
+	def __init__(self, text='', position=(0, 0), **kwargs):
+		super(FileInfoLayer, self).__init__()
+		self.model = None
+		self.background = cocos.layer.util_layers.ColorLayer(250, 0, 0, 250, 1000, 30)
+		self.label = cocos.text.Label(
+			font_name='PT Sans',
+			font_size=14,
+		    color=(250, 250, 250, 255),
+		    anchor_x = "left",
+		    anchor_y = "baseline",
+		    multiline = false
+		)
+		self.add(
+			self.background,
+			self.label
+		)
+
+	def on_slideshow_model_update(self, model):
+		self.model = model
+		self.label.text = self.text
+		print("FileInfoLayer: %s" % self.text)
+
+	@property
+	def text(self):
+		if self.model is None:
+			return ''
+		else:
+			return '[%d  / %d] %s' % \
+		        (self.model.current_id+1, self.model.total_files,
+		        self.model.current_file)
+
+	def draw(self):
+		for c in self.get_children():
+			c.draw()
 
 
 
@@ -675,18 +501,22 @@ class SlideshowModel(event.EventDispatcher):
 
 		)
 
+	def __repr__(self):
+		return "SlideshowModel:" + \
+		       "\n".join([(k, getattr(self, k)) for k in self.__dict__])
+
 SlideshowModel.register_event_type("on_slideshow_model_update")
 
 
 ### Controllers ---------------------------------------------------------------
 
-class SlideshowController(event.EventDispatcher):
+class SlideshowController(cocos.cocosnode.CocosNode):
 	"""controller for slideshow interactions"""
 
-	# started = False
+	is_event_handler = True
 
-	def __init__(self, model):
-		"""init control with model"""
+	def __init__(self, model, *args, **kwargs):
+		super(SlideshowController, self).__init__(*args, **kwargs)
 		self.model = model
 
 	def add_model_observer(self, view):
@@ -696,25 +526,32 @@ class SlideshowController(event.EventDispatcher):
 
 	def on_key_press(self, symbol, modifiers):
 		"""key press event: mostly listening for slideshow navigation"""
-		if symbol in [key.RIGHT, key.LEFT]:
 
-			if symbol == key.RIGHT:
+		print ("SS CONTROL: ON_KEY_PRESS")
+
+		if symbol in [pyglet.window.key.RIGHT, pyglet.window.key.LEFT]:
+
+			if symbol == pyglet.window.key.RIGHT:
 				self.model.next()
-			elif symbol == key.LEFT:
+			elif symbol == pyglet.window.key.LEFT:
 				self.model.prev()
 
 		# slideshow start/stop
-		elif symbol in [key.S, key.SPACE]:
+		elif symbol in [pyglet.window.key.S, pyglet.window.key.SPACE]:
 			self.model.toggle_play()
 
 		# adjust show timing and starts it off if not already playing
 		else:
 			time = 10000
-			timekeys = (key.GRAVE, key._1, key._2, key._3, key._4, key._5,
-						key._6, key._7, key._8, key._9, key._0)
+			timekeys = (pyglet.window.key.GRAVE, pyglet.window.key._1,
+			            pyglet.window.key._2, pyglet.window.key._3,
+			            pyglet.window.key._4, pyglet.window.key._5,
+						pyglet.window.key._6, pyglet.window.key._7,
+						pyglet.window.key._8, pyglet.window.key._9,
+						pyglet.window.key._0)
 			if symbol in timekeys:
 				time = timekeys.index(symbol) * 1000
-			if symbol == key.GRAVE:
+			if symbol == pyglet.window.key.GRAVE:
 				time = 500
 			self.model.change_play_interval(time)
 
@@ -726,19 +563,24 @@ if __name__ == "__main__":
 		width=800, height=600, caption="Image Viewer", fullscreen = False
 	)
 
+	ss_model   = SlideshowModel('/Volumes/Proteus/virtualbox/_share/bru')
+	ss_control = SlideshowController(ss_model)
+
 	scene = cocos.scene.Scene()
 	scene.add(
 		BackgroundLayer(64, 0, 0, 255, width=800, height=600),
 		ImageLayer(),
-		TextWidgetLayer()
+		TextWidgetLayer(),
+		FileInfoLayer()
 	)
 
-	# non=-viuals: data model and controller:
-	ss_model   = SlideshowModel('/Volumes/Proteus/virtualbox/_share/bru')
-	ss_control = SlideshowController(ss_model)
+	director.window.push_handlers()
+
 
 	for c in scene.children:
 		ss_model.push_handlers(c)
+
+	director.window.push_handlers(ss_control)
 
 	director.run(scene)
 
